43472f2a46013cbc689b1952b1648d034721a2b3
Gui Soares <guilhers53292@gmail.com>
2026-02-26 21:22:56 +0000

    layout_basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,11 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+  projeto: any = [
+    { descricao: "Tomar banho", local: "casa", hora_inicio: "19:00", hora_termino: "22:53", status: "em espera" },
+    { descricao: "Beber agua", local: "escola", hora_inicio: "14:40", hora_termino: "17:03", status: "concluido" },
+    { descricao: "Comer pão", local: "casa", hora_inicio: "08:20", hora_termino: "15:10", status: "concluido" },
+    { descricao: "Ir para casa", local: "ônibus", hora_inicio: "18:25", hora_termino: "00:10", status: "em progresso" },
+  ];
 
 }
